6ce917f7351c1e72543194ad70545840c337bb00
kingsleynwafor54 <kingsleynwafor54@gmail.com>
2026-02-10 02:28:45 +0100

    added a main test

--- main.py
+++ main.py
@@ -2,6 +2,10 @@
 def display_hello():
     return "Hello World"
 
+def test_display_hello():
+    assert display_hello() == "Hello World"
+
 
 if __name__=="__main__":
     print(display_hello())
+    
